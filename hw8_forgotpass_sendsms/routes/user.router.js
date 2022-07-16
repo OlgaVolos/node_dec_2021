b0c20a8dbcba6e8685848feb844f7a43d00e93b6
OlgaVolos <72765623+OlgaVolos@users.noreply.github.com>
2022-07-16 18:46:57 +0300

    hw8_correction

--- hw8_forgotpass_sendsms/routes/user.router.js
+++ hw8_forgotpass_sendsms/routes/user.router.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 
-const {commonMiddleware, userMiddleware} = require("../middlewares");
+const {commonMiddleware, userMiddleware, authMiddleware} = require("../middlewares");
 const {userControllers} = require("../controllers");
 
 router.get('/',
@@ -19,12 +19,14 @@ router.get('/:id',
 
 router.put('/:id',
     commonMiddleware.isIdValid,
+    authMiddleware.checkAccessToken, // беремо на всі закриті ендпоінти, на які може переходити лише користувач
     userMiddleware.isUpdatedUserValid,
     userMiddleware.isUserPresent,
     userControllers.updateUser);
 
 router.delete('/:id',
     commonMiddleware.isIdValid,
+    authMiddleware.checkAccessToken, // беремо на всі закриті ендпоінти, на які може переходити лише користувач
     userMiddleware.isUserPresent,
     userControllers.deleteUser);
 
